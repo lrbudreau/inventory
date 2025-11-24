15625277bca9fdeb6f9a4be75d189a9ffb382e72
lrbudreau <100598737+lrbudreau@users.noreply.github.com>
2025-11-24 11:59:25 -0500

    Update API_BASE URL in auth.js

--- frontend/src/auth.js
+++ frontend/src/auth.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://script.google.com/macros/s/AKfycbwngfxfORI0WCKydaj0Gblw5szUZ6-mgWGtcDUEwWbbTaFWg3sWzqv5f2RQUCeyKfR5/exec";
+const API_BASE = "https://script.google.com/macros/s/AKfycbyzyNSnJrfeVyx9m62oh-c6souRBIEOoNd39IleJ1-9FdwwN1-CNT5ahmvtgESbWUZd/exec";
 
 export async function api(path, opts = {}) {
   const headers = opts.headers || {};
